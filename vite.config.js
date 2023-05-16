ea9fbffff995a7e5a57a81c040ac7a779a827074
pabloozamora <pabloozamora02@gmail.com>
2023-05-15 20:00:41 -0600

    Lab Finalizado

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "http://uvgenios.online/21780/Lab_7/"
 })
 
 resolve: {
   alias: []
 }
+
